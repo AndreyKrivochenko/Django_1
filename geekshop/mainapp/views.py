e8a300f9468dfff72850ffc9e79f21458a281b7f
Andrey Krivochenko <krivochenko_andrey@mail.ru>
2021-06-25 20:26:09 +0700

    Lesson 2 homework complit

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -9,9 +9,16 @@ links_menu = [
         {'name': 'классика'},
 ]
 
+main_menu = [
+    {'href': 'index', 'name': 'домой'},
+    {'href': 'products:index', 'name': 'продукты'},
+    {'href': 'contacts', 'name': 'контакты'},
+]
+
 content = {
     'title': title,
     'links_menu': links_menu,
+    'main_menu': main_menu,
 }
 
 
