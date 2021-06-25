77a52d1fe4c11cc63e31d69bae0188b8a401a84c
Andrey Krivochenko <krivochenko_andrey@mail.ru>
2021-06-25 15:33:50 +0700

    Домашнее задание на 90%

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+title = 'каталог'
+links_menu = [
+        {'name': 'все'},
+        {'name': 'дом'},
+        {'name': 'офис'},
+        {'name': 'модерн'},
+        {'name': 'классика'},
+]
+
+content = {
+    'title': title,
+    'links_menu': links_menu,
+}
+
 
 def products(request):
-    return render(request, 'products.html')
+    return render(request, 'products.html', context=content)
