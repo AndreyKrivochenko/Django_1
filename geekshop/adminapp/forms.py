2e0336d1348293eeb5d446fe7f25e38612d3b42a
Andrey Krivochenko <krivochenko_andrey@mail.ru>
2021-07-23 01:14:28 +0700

    lesson7

---
+++ geekshop/adminapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from authapp.models import ShopUser
+from authapp.forms import ShopUserEditForm
+from mainapp.models import ProductCategory
+
+
+class ShopUserAdminEditForm(ShopUserEditForm):
+    class Meta:
+        model = ShopUser
+        fields = '__all__'
+
+
+class CategoryAdminCreateForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ('name', 'description')
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -69,6 +69,6 @@ def basket_edit(request, pk, quantity):
             'basket': _basket,
         }
 
-        result = render_to_string('basketapp/includes/inc_basket_list.html', context=context)
+        result = render_to_string('basketapp/inсludes/inc_basket_list.html', context=context)
 
         return JsonResponse({'result': result})
