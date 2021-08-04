1830738bae6dc4a1380c62d931c864d0b37b8981
Andrey Krivochenko <krivochenko_andrey@mail.ru>
2021-08-04 16:22:25 +0700

    Lesson2.4_1

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
+
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, pre_delete
+
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -122,3 +126,21 @@ def order_forming_complete(request, pk):
    order.save()
 
    return HttpResponseRedirect(reverse('ordersapp:orders_list'))
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if update_fields is 'quantity' or 'product':
+        if instance.pk:
+            instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        else:
+            instance.product.quantity += instance.quantity
+        instance.save()
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
