f107c41cb8ef4208aec01efad21e65e34ebe6eae
root <root@194-58-104-108.cloudvps.regruhosting.ru>
2021-08-14 19:43:04 +0300

    cache

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -6,6 +6,76 @@ from basketapp.models import Basket
 from .models import ProductCategory, Product
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.conf import settings
+from django.core.cache import cache
+
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_deleted=False)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_deleted=False)
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_deleted=False, category__is_deleted=False).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_deleted=False, category__is_deleted=False).select_related('category')
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+def get_products_oreded_by_price():
+    if settings.LOW_CACHE:
+        key = 'products_orederd_by_price'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_deleted=False, category__is_deleted=False).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_deleted=False, category__is_deleted=False).order_by('price')
+
+def get_products_in_category_oreded_by_price(pk):
+   if settings.LOW_CACHE:
+       key = f'products_in_category_oreded_by_price_{pk}'
+       products = cache.get(key)
+       if products is None:
+           products = Product.objects.filter(category__pk=pk, is_deleted=False, category__is_deleted=False).order_by('price')
+           cache.set(key, products)
+       return products
+   else:
+       return Product.objects.filter(category__pk=pk, is_deleted=False, category__is_deleted=False).order_by('price')
+
+
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
 
 def get_basket(user):
     if user.is_authenticated:
@@ -15,7 +85,7 @@ def get_basket(user):
 
 
 def get_hot_product():
-    products = Product.objects.all()
+    products = get_products()
     return sample(list(products), 1)[0]
 
 
@@ -28,7 +98,7 @@ def products(request, pk=None, page=1):
 
     title = 'каталог'
 
-    links_menu = ProductCategory.objects.all()
+    links_menu = get_links_menu()
 
     main_menu = [
         {'href': 'index', 'name': 'домой'},
@@ -38,11 +108,11 @@ def products(request, pk=None, page=1):
 
     if pk is not None:
         if pk == 0:
-            products = Product.objects.filter(is_deleted=False).order_by('price')
+            products = get_products_oreded_by_price()
             category = {'pk': 0, 'name': 'все'}
         else:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(is_deleted=False, category__pk=pk).order_by('price')
+            category = get_category(pk=pk)
+            products = get_products_in_category_oreded_by_price(pk=pk)
 
         paginator = Paginator(products, 2)
 
@@ -84,8 +154,8 @@ def product(request, pk):
 
     context = {
         'title': title,
-        'links_menu': ProductCategory.objects.all(),
-        'product': get_object_or_404(Product, pk=pk),
+        'links_menu': get_links_menu(),
+        'product': get_product(pk=pk),
         'basket': get_basket(request.user),
     }
 
